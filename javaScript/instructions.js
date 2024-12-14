85b39325c8de101c7b16d6c401bf28c8bf6962b1
KRyderTR <charitrom@gmail.com>
2024-12-14 16:40:47 +0200

    menu-icons-update2

--- javaScript/instructions.js
+++ javaScript/instructions.js
@@ -1,14 +1,20 @@
 // Get references to the elements
-const instructionsButton = document.getElementById('instructions-btn');
+const instructionsButton = document.getElementById('rightImg');
 const instructionsModal = document.getElementById('instructions-modal');
+const overlay = document.getElementById("modal-overlay");
 const closeInstructionsButton = document.getElementById('close-instructions-btn');
 
 // Show the instructions modal when the button is clicked
 instructionsButton.addEventListener('click', () => {
   instructionsModal.style.display = 'flex'; // Show the modal
+  overlay.style.display = 'block';
 });
 
 // Hide the instructions modal when the close button is clicked
 closeInstructionsButton.addEventListener('click', () => {
   instructionsModal.style.display = 'none'; // Hide the modal
+  overlay.style.display = "none";
 });
+
+
+
--- javaScript/modal.js
+++ javaScript/modal.js
@@ -3,16 +3,20 @@ const modal = document.getElementById("game-modal");
 const closeButton = document.querySelector(".close-button");
 const okButton = document.getElementById("modal-ok-button");
 const modalMessage = document.getElementById("modal-message");
+const gameOverlay = document.getElementById("modal-overlay");
 
 const showModal = (message, restart = false) => {
-    modalMessage.textContent = message;
-    modal.style.display = "block";
+  modalMessage.textContent = message;
+  modal.style.display = "block";
+  gameOverlay.style.display = "block";
 
-    okButton.onclick = () => {
-        modal.style.display = "none";
-        if (restart) {startGame();} 
-        else {displaySequence();}
-    };
+  okButton.onclick = () => {
+    modal.style.display = "none";
+    gameOverlay.style.display = "none";
+    if (restart) {
+      startGame();
+    } else {
+      displaySequence();
+    }
+  };
 };
-
-
